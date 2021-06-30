6e2558b8aee2301fd90db442f2fcbe8b6c40baa6
Jose Narvaez <jnarvaez>
2021-06-29 23:06:55 -0400

    Cambios varios

--- portafolio/src/main/java/com/feriantes/portafolio/homeClienteController.java
+++ portafolio/src/main/java/com/feriantes/portafolio/homeClienteController.java
@@ -2,7 +2,9 @@ package com.feriantes.portafolio;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,25 +18,42 @@ import com.feriantes.portafolio.to.ProcesoTO;
 
 @Controller
 public class homeClienteController {
-	
+
 	@Autowired
-    private PerfilesService PerfilesService;
-	
+	private PerfilesService PerfilesService;
+
 	@Autowired
 	private ProcesoDao ProcesoDao;
-	
-	
-    @GetMapping("/homeCliente")
-    public String nuevoProceso(Model model, @AuthenticationPrincipal UserDetails userDetails){
-        PerfilesService.seteaPerfil(model, userDetails);
-        List<ProcesoTO> list = new ArrayList<>();
-        try {
-        	list = ProcesoDao.obtenerProceso();
+
+	@GetMapping("/homeCliente")
+	public String nuevoProceso(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+		PerfilesService.seteaPerfil(model, userDetails);
+		List<ProcesoTO> list = new ArrayList<>();
+		try {
+			list = ProcesoDao.obtenerProceso();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-        model.addAttribute("procesos", list);
-        return "homeCliente";
-    }
-    
+
+		Map<Integer, List<ProcesoTO>> map = new HashMap<>();
+		int contador = 1;
+		List<ProcesoTO> listProceso = new ArrayList<>();
+		for (ProcesoTO procesoTO : list) {
+
+			listProceso.add(procesoTO);
+			if (contador == 3) {
+				contador = 1;
+				map.put(procesoTO.getIdProceso(), listProceso);
+				listProceso = new ArrayList<>();
+				continue;
+			}
+			contador++;
+		}
+		if(!listProceso.isEmpty()) {
+			map.put(-9999, listProceso);
+		}
+		model.addAttribute("procesos", map);
+		return "homeCliente";
+	}
+
 }
--- portafolio/src/main/java/com/feriantes/portafolio/to/ProcesoTO.java
+++ portafolio/src/main/java/com/feriantes/portafolio/to/ProcesoTO.java
@@ -4,73 +4,95 @@ import java.util.List;
 
 public class ProcesoTO {
 
-    private int idProceso;
-    private int estadoProceso;
-    private String nombreProceso;
-    private String fechaInicio;
-    private String fechaTermino;
-    private String funcion;
-    private String urlDetalle;
-    private List<DetalleProcesoTO> listaDetalleProceso;
-    private String glosaEstado;
+	private int idProceso;
+	private int estadoProceso;
+	private String nombreProceso;
+	private String fechaInicio;
+	private String fechaTermino;
+	private String funcion;
+	private String urlDetalle;
+	private List<DetalleProcesoTO> listaDetalleProceso;
+	private String glosaEstado;
+	private String descripcion;
 
-    
+	public String getGlosaEstado() {
+		return glosaEstado;
+	}
 
-    public String getGlosaEstado() {
-        return glosaEstado;
-    }
-    public void setGlosaEstado(String glosaEstado) {
-        this.glosaEstado = glosaEstado;
-    }
-    public List<DetalleProcesoTO> getListaDetalleProceso() {
-        return listaDetalleProceso;
-    }
-    public void setListaDetalleProceso(List<DetalleProcesoTO> listaDetalleProceso) {
-        this.listaDetalleProceso = listaDetalleProceso;
-    }
-    public String getUrlDetalle() {
-        return urlDetalle;
-    }
-    public void setUrlDetalle(String urlDetalle) {
-        this.urlDetalle = urlDetalle;
-    }
-    public int getIdProceso() {
-        return idProceso;
-    }
-    public void setIdProceso(int idProceso) {
-        this.idProceso = idProceso;
-    }
-    public int getEstadoProceso() {
-        return estadoProceso;
-    }
-    public void setEstadoProceso(int estadoProceso) {
-        this.estadoProceso = estadoProceso;
-    }
-    public String getNombreProceso() {
-        return nombreProceso;
-    }
-    public void setNombreProceso(String nombreProceso) {
-        this.nombreProceso = nombreProceso;
-    }
-    public String getFechaInicio() {
-        return fechaInicio;
-    }
-    public void setFechaInicio(String fechaInicio) {
-        this.fechaInicio = fechaInicio;
-    }
-    public String getFechaTermino() {
-        return fechaTermino;
-    }
-    public void setFechaTermino(String fechaTermino) {
-        this.fechaTermino = fechaTermino;
-    }
-    public String getFuncion() {
-        return funcion;
-    }
-    public void setFuncion(String funcion) {
-        this.funcion = funcion;
-    }
+	public void setGlosaEstado(String glosaEstado) {
+		this.glosaEstado = glosaEstado;
+	}
 
+	public List<DetalleProcesoTO> getListaDetalleProceso() {
+		return listaDetalleProceso;
+	}
 
-   
+	public void setListaDetalleProceso(List<DetalleProcesoTO> listaDetalleProceso) {
+		this.listaDetalleProceso = listaDetalleProceso;
+	}
+
+	public String getUrlDetalle() {
+		return urlDetalle;
+	}
+
+	public void setUrlDetalle(String urlDetalle) {
+		this.urlDetalle = urlDetalle;
+	}
+
+	public int getIdProceso() {
+		return idProceso;
+	}
+
+	public void setIdProceso(int idProceso) {
+		this.idProceso = idProceso;
+	}
+
+	public int getEstadoProceso() {
+		return estadoProceso;
+	}
+
+	public void setEstadoProceso(int estadoProceso) {
+		this.estadoProceso = estadoProceso;
+	}
+
+	public String getNombreProceso() {
+		return nombreProceso;
+	}
+
+	public void setNombreProceso(String nombreProceso) {
+		this.nombreProceso = nombreProceso;
+	}
+
+	public String getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(String fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public String getFechaTermino() {
+		return fechaTermino;
+	}
+
+	public void setFechaTermino(String fechaTermino) {
+		this.fechaTermino = fechaTermino;
+	}
+
+	public String getFuncion() {
+		return funcion;
+	}
+
+	public void setFuncion(String funcion) {
+		this.funcion = funcion;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
 }
